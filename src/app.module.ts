b29dc271b39f3bc460368878238ddc5d7ffeb244
Reza Amya <rezaamya@gmail.com>
2024-08-14 14:08:48 +0330

    feat: Initialize config globally for app and postgres using `.env` and `@nestjs/config`

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,18 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import postgresConfig from './configs/postgres.config';
+import appConfig from './configs/app.config';
+import { configValidator } from './configs/config.validation';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      cache: true,
+      load: [appConfig, postgresConfig],
+      validate: configValidator,
+    }),
+  ],
   controllers: [],
   providers: [],
 })
