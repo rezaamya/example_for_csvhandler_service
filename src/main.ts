2f15d042d02a3551894f2b90695563560599ded7
Reza Amya <rezaamya@gmail.com>
2024-08-15 13:00:19 +0330

    feat: validation and transformation pipe enabled for all incoming requests

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,16 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
 import { IAppConfig } from './configs/app.config';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+    }),
+  );
+
   const configService = app.get<ConfigService>(ConfigService);
   const appConfig = configService.get<IAppConfig>('app');
 
